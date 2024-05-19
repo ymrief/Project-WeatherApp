da4f0e96491e4aa2ca2ff8754bdc26ca0b402921
Yusuf Martinus Arief <brothanes11@gmail.com>
2024-05-19 17:04:15 +0700

    Adding weather page, weather model, and weather service

---
+++ lib/pages/weather_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// stf -> Flutter Stateful Widget
+
+class WeatherPage extends StatefulWidget {
+  const WeatherPage({super.key});
+
+  @override
+  State<WeatherPage> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
